47958a18dba5e3f6748967ecafa2cb4945752bc8
Nick Dyer <nick.dyer@atos.net>
2015-08-07 18:54:06 +0100

    Adds tests for issuing references passing

--- lib/story.rb
+++ lib/story.rb
@@ -1,18 +1,24 @@
 class Story
 
   attr_reader :content, :story
+  attr_accessor :parent_reference
 
   def initialize
     @story = Hash.new
     @content = [] 
+    @parent_reference = ""
   end
 
-  def add_line(sentence)
-    @story["line1"] = sentence
+  def add_line(sentence, reference)
+    @story[reference] = sentence
     check_content
   end
 
   def check_content
     @content = @story.values
+  end
+
+  def retrieve_line(reference)
+    @story[reference]
   end
 end
--- spec/story_spec.rb
+++ spec/story_spec.rb
@@ -2,15 +2,32 @@ require 'story'
 
 describe Story do
 
-  it "can be initialised with no content" do
+  it "is initialised with no content" do
     story = Story.new()
     expect(story.content).to be_empty
   end
 
+  it "is initialised with no parent line reference" do
+    story = Story.new()
+    expect(story.parent_reference).to eq ""
+  end
+
   it "can have a line added to it" do
     story = Story.new()
-    story.add_line "Once upon a time..."
+    story.add_line("Once upon a time...", "A")
     expect(story.content).to include "Once upon a time..."
+  end
+
+  it "can retrieve a line" do
+    story = Story.new()
+    story.add_line("Once upon a time...", "A")
+    expect(story.retrieve_line("A")).to eq "Once upon a time..."
+  end
+
+  it "can have a line reference issued to it" do
+    story = Story.new()
+    story.parent_reference = "B"
+    expect(story.parent_reference).to eq "B"
   end
 
 end
