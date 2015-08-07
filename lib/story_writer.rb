640a6de7ccc87235f6632c63a6cec96e21013848
Nick Dyer <nick.dyer@atos.net>
2015-08-07 20:47:48 +0100

    Renaming of input boxes has broken tests, but otherwise working

--- lib/story_writer.rb
+++ lib/story_writer.rb
@@ -4,24 +4,45 @@ require_relative 'story'
 class StoryWriter < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "..", "views") }
+  set :public_folder, Proc.new { File.join(root, "..", "public") }
 
+  enable :sessions
   STORY = Story.new
 
   get '/' do
+    STORY = Story.new
     erb :index
   end
 
-  get '/create_story' do
-    STORY.parent_reference = "0"
+  post '/create_story' do
+    session[:story_id] = STORY.object_id
+    @child_lines = []
     erb :story
   end
 
-  post '/create_story' do
-    @newline = params["input-box-0"]
-    @phrase1 = params["input-box-1"]
-    @phrase2 = params["input-box-2"]
-    @phrase3 = params["input-box-3"]
-    @phrase4 = params["input-box-4"]
+  post '/first_line' do
+    p session
+    STORY.add_line(params["newline"], params["reference"])
+    STORY.parent_reference = "0"
+    p STORY.story
+    @current_line = STORY.retrieve_line(params["reference"])
+    @child_lines = []
+    erb :story
+  end
+
+  post '/add_line' do
+    p session
+    STORY.add_line(params["newline"], params["reference"])
+    p STORY.story
+    @current_line = STORY.retrieve_line(STORY.parent_reference)
+    @child_lines = STORY.collect_child_lines_of(STORY.parent_reference)
+    erb :story
+  end
+
+  post '/select_line' do
+    p session
+    @current_line = STORY.retrieve_line(params["reference"])
+    @child_lines = STORY.collect_child_lines_of(params["reference"])
     erb :story
   end
 
