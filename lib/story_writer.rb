e529cd1ea2d1e3645e768e2bd595fd6c5e70bdf8
Nick Dyer <nick.dyer@atos.net>
2015-08-09 21:55:14 +0100

    Changes STORY from constant to global variable to be accurate when reinitiating story

--- lib/story_writer.rb
+++ lib/story_writer.rb
@@ -1,48 +1,51 @@
 require 'sinatra/base'
 require_relative 'story'
+#require_relative 'helpers'
 
 class StoryWriter < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "..", "views") }
   set :public_folder, Proc.new { File.join(root, "..", "public") }
 
-  STORY = Story.new
+  @@story = Story.new
 
   get '/' do
     erb :index
   end
 
   get '/stories/1' do
-    STORY.parent_reference = "0"
-    @current_line = STORY.retrieve_line(STORY.parent_reference)
-    @child_lines = STORY.collect_child_lines_of(STORY.parent_reference)
+    @@story.parent_reference = "0"
+    @current_line = @@story.retrieve_line(@@story.parent_reference)
+    @child_lines = @@story.collect_child_lines_of(@@story.parent_reference)
     erb :story
   end
 
   post '/stories/new' do
+    @@story = Story.new
     @child_lines = []
     erb :story
   end
 
+
   post '/first_line' do
-    STORY.add_line(params["newline"], params["reference"])
-    STORY.parent_reference = "0"
-    @current_line = STORY.retrieve_line(params["reference"])
-    @child_lines = []
+    @@story.add_line(params["newline"], params["reference"])
+    @@story.parent_reference = "0"
+    @current_line = @@story.retrieve_line(params["reference"])
+    @child_lines = @@story.collect_child_lines_of(@@story.parent_reference)
     erb :story
   end
 
   post '/lines/new' do
-    STORY.add_line(params["newline"], params["reference"])
-    @current_line = STORY.retrieve_line(STORY.parent_reference)
-    @child_lines = STORY.collect_child_lines_of(STORY.parent_reference)
+    @@story.add_line(params["newline"], params["reference"])
+    @current_line = @@story.retrieve_line(@@story.parent_reference)
+    @child_lines = @@story.collect_child_lines_of(@@story.parent_reference)
     erb :story
   end
 
   get '/lines/:id' do
-    STORY.parent_reference = STORY.parent_reference + params[:id]
-    @current_line = STORY.retrieve_line(STORY.parent_reference)
-    @child_lines = STORY.collect_child_lines_of(STORY.parent_reference)
+    @@story.parent_reference = @@story.parent_reference + params[:id]
+    @current_line = @@story.retrieve_line(@@story.parent_reference)
+    @child_lines = @@story.collect_child_lines_of(@@story.parent_reference)
     erb :story
   end
 
